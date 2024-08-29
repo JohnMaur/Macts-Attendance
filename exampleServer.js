e187a9e8648d8a62b4a2450611417d67f9456b30
John Maur <johnmaur8@gmai.com>
2024-08-29 23:46:41 +0800

    Fixed Attendance insertion of record

--- exampleServer.js
+++ exampleServer.js
@@ -105,69 +105,7 @@ io.on('connection', (socket) => {
   });
 });
 
-// Object to store RFID tag data with timestamp
-const tagDataMap = {};
-
-// Route to handle POST requests to /tagData
-app.post('/tagData', async (req, res) => {
-  const tagData = req.body.tagData;
-
-  console.log('Received tag data via POST:', tagData);
-
-  const now = new Date().getTime();
-  const timeout = 60000; // 1 minute in milliseconds
-
-  if (tagDataMap[tagData] && now - tagDataMap[tagData] < timeout) {
-    console.log("You've already tapped your RFID card. Please wait for a minute before tapping again.");
-    io.emit('tagData', { tagData, excessiveTap: true });
-    return res.status(429).send({ message: "You've already tapped your RFID card. Please wait for a minute before tapping again." });
-  }
-
-  tagDataMap[tagData] = now;
-
-  // Fetch student information from the MySQL server
-  try {
-    // const response = await axios.get('https://macts-backend-webapp-production-0bd2.up.railway.app/studentInfo');
-    const response = await axios.get('https://macts-backend-webapp.onrender.com/studentInfo');
-    const students = response.data;
-    const formattedDate = moment().tz('Asia/Manila').format('M/D/YYYY, h:mm:ss A'); // Format the date in Philippine Time
-
-    // Check for matching tagValue and attendance_code
-    const matchedStudent = students.find(student => student.tagValue === tagData && student.attendance_code);
-
-    if (matchedStudent) {
-      console.log('RFID tag matched:', matchedStudent);
-
-      // Insert matched student information into attendance_taphistory
-      const attendanceHistory = {
-        firstName: matchedStudent.studentInfo_first_name,
-        middleName: matchedStudent.studentInfo_middle_name,
-        lastName: matchedStudent.studentInfo_last_name,
-        tuptId: matchedStudent.studentInfo_tuptId,
-        course: matchedStudent.studentInfo_course,
-        section: matchedStudent.studentInfo_section,
-        email: matchedStudent.user_email,
-        code: matchedStudent.attendance_code,
-        date: formattedDate,
-        user_id: matchedStudent.user_id
-      };
-
-      // await axios.post('https://macts-backend-webapp-production-0bd2.up.railway.app/attendance_taphistory', attendanceHistory);
-      await axios.post('https://macts-backend-webapp.onrender.com/attendance_taphistory', attendanceHistory);
-      // await axios.post('http://192.168.100.138:2525/attendance_taphistory', attendanceHistory);
-      console.log('Tap history recorded successfully');
-    } else {
-      console.log('No matching RFID tag found or attendance code is empty');
-    }
-
-    io.emit('tagData', { tagData, excessiveTap: false });
-  } catch (error) {
-    console.error('Error fetching student information or recording tap history:', error);
-  }
-
-  res.send('Tag data received successfully');
-});
-
+// // Object to store RFID tag data with timestamp
 // const tagDataMap = {};
 
 // // Route to handle POST requests to /tagData
@@ -189,8 +127,9 @@ app.post('/tagData', async (req, res) => {
 
 //   // Fetch student information from the MySQL server
 //   try {
-//     const studentResponse = await axios.get('https://macts-backend-webapp-production-0bd2.up.railway.app/studentInfo');
-//     const students = studentResponse.data;
+//     // const response = await axios.get('https://macts-backend-webapp-production-0bd2.up.railway.app/studentInfo');
+//     const response = await axios.get('https://macts-backend-webapp.onrender.com/studentInfo');
+//     const students = response.data;
 //     const formattedDate = moment().tz('Asia/Manila').format('M/D/YYYY, h:mm:ss A'); // Format the date in Philippine Time
 
 //     // Check for matching tagValue and attendance_code
@@ -199,38 +138,23 @@ app.post('/tagData', async (req, res) => {
 //     if (matchedStudent) {
 //       console.log('RFID tag matched:', matchedStudent);
 
-//       // Fetch the attendance date
-//       const attendanceResponse = await axios.get('https://macts-backend-webapp-production-0bd2.up.railway.app/attendance');
-//       const attendance = attendanceResponse.data;
-
-//       // Assuming the attendance data contains the relevant date information
-//       const attendanceDate = new Date(attendance.attendance_date); // Convert the attendance_date to a Date object
-//       const tapTime = new Date(); // Get the current tap time
-
-//       // Determine status based on tap time and attendance date
-//       let status = 'Present';
-//       if (tapTime > attendanceDate) {
-//         status = 'Late';
-//       }
-
 //       // Insert matched student information into attendance_taphistory
 //       const attendanceHistory = {
-//         attendance_firstName: matchedStudent.studentInfo_first_name,
-//         attendance_middleName: matchedStudent.studentInfo_middle_name,
-//         attendance_Lastname: matchedStudent.studentInfo_last_name,
-//         attendance_tupId: matchedStudent.studentInfo_tuptId,
-//         attendance_course: matchedStudent.studentInfo_course,
-//         attendance_section: matchedStudent.studentInfo_section,
-//         attendance_email: matchedStudent.user_email,
-//         attendance_historyDate: formattedDate,
-//         attendance_code: matchedStudent.attendance_code,
-//         user_id: matchedStudent.user_id,
-//         status: status // Insert status based on tap time
+//         firstName: matchedStudent.studentInfo_first_name,
+//         middleName: matchedStudent.studentInfo_middle_name,
+//         lastName: matchedStudent.studentInfo_last_name,
+//         tuptId: matchedStudent.studentInfo_tuptId,
+//         course: matchedStudent.studentInfo_course,
+//         section: matchedStudent.studentInfo_section,
+//         email: matchedStudent.user_email,
+//         code: matchedStudent.attendance_code,
+//         date: formattedDate,
+//         user_id: matchedStudent.user_id
 //       };
 
 //       // await axios.post('https://macts-backend-webapp-production-0bd2.up.railway.app/attendance_taphistory', attendanceHistory);
-//       await axios.post('http://192.168.100.138:2525/attendance_taphistory', attendanceHistory);
-//       console.log('Tap history recorded successfully with status:', status);
+//       await axios.post('https://macts-backend-webapp.onrender.com/attendance_taphistory', attendanceHistory);
+//       console.log('Tap history recorded successfully');
 //     } else {
 //       console.log('No matching RFID tag found or attendance code is empty');
 //     }
@@ -242,6 +166,83 @@ app.post('/tagData', async (req, res) => {
 
 //   res.send('Tag data received successfully');
 // });
+
+const tagDataMap = {};
+
+// Route to handle POST requests to /tagData
+app.post('/tagData', async (req, res) => {
+  const tagData = req.body.tagData;
+
+  console.log('Received tag data via POST:', tagData);
+
+  const now = new Date().getTime();
+  const timeout = 60000; // 1 minute in milliseconds
+
+  if (tagDataMap[tagData] && now - tagDataMap[tagData] < timeout) {
+    console.log("You've already tapped your RFID card. Please wait for a minute before tapping again.");
+    io.emit('tagData', { tagData, excessiveTap: true });
+    return res.status(429).send({ message: "You've already tapped your RFID card. Please wait for a minute before tapping again." });
+  }
+
+  tagDataMap[tagData] = now;
+
+  // Fetch student information from the MySQL server
+  try {
+    // const studentResponse = await axios.get('https://macts-backend-webapp-production-0bd2.up.railway.app/studentInfo');
+    const studentResponse = await axios.get('https://macts-backend-webapp.onrender.com/studentInfo');
+    const students = studentResponse.data;
+    const formattedDate = moment().tz('Asia/Manila').format('M/D/YYYY, h:mm:ss A'); // Format the date in Philippine Time
+
+    // Check for matching tagValue and attendance_code
+    const matchedStudent = students.find(student => student.tagValue === tagData && student.attendance_code);
+
+    if (matchedStudent) {
+      console.log('RFID tag matched:', matchedStudent);
+
+      // Fetch the attendance date
+      // const attendanceResponse = await axios.get('https://macts-backend-webapp-production-0bd2.up.railway.app/attendance');
+      const attendanceResponse = await axios.get('https://macts-backend-webapp.onrender.com/attendance');
+      const attendance = attendanceResponse.data;
+
+      // Assuming the attendance data contains the relevant date information
+      const attendanceDate = new Date(attendance.attendance_date); // Convert the attendance_date to a Date object
+      const tapTime = new Date(); // Get the current tap time
+
+      // Determine status based on tap time and attendance date
+      let status = 'Present';
+      if (tapTime > attendanceDate) {
+        status = 'Late';
+      }
+
+      // Insert matched student information into attendance_taphistory
+      const attendanceHistory = {
+        attendance_firstName: matchedStudent.studentInfo_first_name,
+        attendance_middleName: matchedStudent.studentInfo_middle_name,
+        attendance_Lastname: matchedStudent.studentInfo_last_name,
+        attendance_tupId: matchedStudent.studentInfo_tuptId,
+        attendance_course: matchedStudent.studentInfo_course,
+        attendance_section: matchedStudent.studentInfo_section,
+        attendance_email: matchedStudent.user_email,
+        attendance_historyDate: formattedDate,
+        attendance_code: matchedStudent.attendance_code,
+        user_id: matchedStudent.user_id,
+        status: status // Insert status based on tap time
+      };
+
+      // await axios.post('https://macts-backend-webapp-production-0bd2.up.railway.app/attendance_taphistory', attendanceHistory);
+      await axios.post('https://macts-backend-webapp.onrender.com/attendance_taphistory', attendanceHistory);
+      console.log('Tap history recorded successfully with status:', status);
+    } else {
+      console.log('No matching RFID tag found or attendance code is empty');
+    }
+
+    io.emit('tagData', { tagData, excessiveTap: false });
+  } catch (error) {
+    console.error('Error fetching student information or recording tap history:', error);
+  }
+
+  res.send('Tag data received successfully');
+});
 
 
 // Koa setup
